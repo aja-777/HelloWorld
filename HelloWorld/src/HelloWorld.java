140b28f46606c3d7b0555341658d06af0cc3e1c0
aja-777 <almanza.aj@outlook.com>
2024-09-22 21:35:34 +0200

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("hola mundo");
+		System.out.println("hola mundo mundo");
 		// TODO Auto-generated method stub
 
 	}
